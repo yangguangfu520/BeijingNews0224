60dc1a0b5bb8f7ed788811d8539708eb224b477b
yangguangfu520 <ygf@atguigu.com>
2017-06-03 09:33:25 +0800

    屏蔽指定页面不能拖拽出菜单

--- app/src/main/java/com/atguigu/myapplication/base/BaseFragment.java
+++ app/src/main/java/com/atguigu/myapplication/base/BaseFragment.java
@@ -22,7 +22,7 @@ public abstract class BaseFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        context = getActivity();
+        context = getActivity();//MainActivity
     }
 
     /**
--- app/src/main/java/com/atguigu/myapplication/fragment/ContentFragment.java
+++ app/src/main/java/com/atguigu/myapplication/fragment/ContentFragment.java
@@ -1,17 +1,21 @@
 package com.atguigu.myapplication.fragment;
 
+import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioGroup;
 
 import com.atguigu.myapplication.R;
+import com.atguigu.myapplication.activity.MainActivity;
 import com.atguigu.myapplication.base.BaseFragment;
 import com.atguigu.myapplication.base.BasePager;
 import com.atguigu.myapplication.pager.HomePager;
 import com.atguigu.myapplication.pager.NewsPager;
 import com.atguigu.myapplication.pager.SettingPager;
 import com.atguigu.myapplication.view.NoViewPager;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 
 import java.util.ArrayList;
 
@@ -66,40 +70,59 @@ public class ContentFragment extends BaseFragment {
                         //ViewPager.setCurrentItem(position)
                         vp.setCurrentItem(0,false);
                         //调用initData方法
-                        pagers.get(0).initData();
+//                        pagers.get(0).initData();
                         break;
                     case R.id.rb_news://新闻
                         vp.setCurrentItem(1,false);
-                        pagers.get(1).initData();
+//                        pagers.get(1).initData();
                         break;
                     case R.id.rb_setting://设置
                         vp.setCurrentItem(2,false);
-                        pagers.get(2).initData();
+//                        pagers.get(2).initData();
                         break;
                 }
             }
         });
 
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
-//        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-//            @Override
-//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//
-//            }
-//
-//            @Override
-//            public void onPageSelected(int position) {
-//                pagers.get(position).initData();
-//            }
-//
-//            @Override
-//            public void onPageScrollStateChanged(int state) {
-//
-//            }
-//        });
-//        pagers.get(0).initData();
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                pagers.get(position).initData();
+                if(position ==1){
+                    //可以侧滑
+                    isEnableSlidingMenu(context, SlidingMenu.TOUCHMODE_FULLSCREEN);
+                }else{
+                    //不可用侧滑
+                    isEnableSlidingMenu((MainActivity) context, SlidingMenu.TOUCHMODE_NONE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        pagers.get(0).initData();
         //默认选中主页
         rgMain.check(R.id.rb_home);
+
+        //不可用侧滑
+        isEnableSlidingMenu((MainActivity) context, SlidingMenu.TOUCHMODE_NONE);
+    }
+
+    /**
+     * 是否让SlidingMenu可以滑动
+     * @param context
+     * @param touchmodeFullscreen
+     */
+    private static void isEnableSlidingMenu(Context context, int touchmodeFullscreen) {
+        MainActivity mainActivity = (MainActivity) context;
+        mainActivity.getSlidingMenu().setTouchModeAbove(touchmodeFullscreen);
     }
 
     class MyAdapter extends PagerAdapter{
