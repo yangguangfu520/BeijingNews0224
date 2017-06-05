9f51f9fea7857e9301f7e2dd84dc57df57d16c87
yangguangfu520 <ygf@atguigu.com>
2017-06-05 11:52:00 +0800

    配置联网并且请求&解析数据

--- app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
@@ -2,6 +2,7 @@ package com.atguigu.myapplication.detailpager;
 
 import android.content.Context;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -10,9 +11,15 @@ import android.widget.TextView;
 import com.atguigu.myapplication.R;
 import com.atguigu.myapplication.base.MenuDetailBasePager;
 import com.atguigu.myapplication.domain.NewsCenterBean;
+import com.atguigu.myapplication.domain.TabDetailPagerBean;
+import com.atguigu.myapplication.utils.ConstantUtils;
+import com.google.gson.Gson;
+import com.zhy.http.okhttp.OkHttpUtils;
+import com.zhy.http.okhttp.callback.StringCallback;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
+import okhttp3.Call;
 
 /**
  * 作者：杨光福 on 2017/6/5 09:24
@@ -32,6 +39,8 @@ public class TabDetailPager extends MenuDetailBasePager {
     @InjectView(R.id.lv)
     ListView lv;
 
+    private String url;
+
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
         this.childrenBean = childrenBean;
@@ -40,7 +49,7 @@ public class TabDetailPager extends MenuDetailBasePager {
     @Override
     public View initView() {
         View view = View.inflate(context, R.layout.pager_tab_detail, null);
-        ButterKnife.inject(this,view);
+        ButterKnife.inject(this, view);
         //创建子类的视图
         return view;
     }
@@ -48,7 +57,42 @@ public class TabDetailPager extends MenuDetailBasePager {
     @Override
     public void initData() {
         super.initData();
+        url = ConstantUtils.BASE_URL + childrenBean.getUrl();
+        Log.e("TAG","url=="+url);
         //设置数据
-        tvTitle.setText(childrenBean.getTitle());
+        getDataFromNet();
     }
+
+    private void getDataFromNet() {
+        OkHttpUtils
+                .get()
+                .url(url)
+//                .addParams("username", "hyman")
+//                .addParams("password", "123")
+                .build()
+                .execute(new StringCallback() {
+                    @Override
+                    public void onError(Call call, Exception e, int id) {
+                        Log.e("TAG", "请求失败==" + e.getMessage());
+
+                    }
+
+                    @Override
+                    public void onResponse(String response, int id) {
+//                        Log.e("TAG", "请求成功==" + response);
+                        //缓存数据
+                        processData(response);
+                    }
+
+
+                });
+    }
+
+    private void processData(String response) {
+        TabDetailPagerBean bean = new Gson().fromJson(response,TabDetailPagerBean.class);
+        ;
+        Log.e("TAG",""+bean.getData().getNews().get(0).getTitle());
+    }
+
+
 }
