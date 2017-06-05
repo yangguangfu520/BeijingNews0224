270528f679f0d6b60a94b0b1ca956aabc631248c
yangguangfu520 <ygf@atguigu.com>
2017-06-05 11:40:40 +0800

    页签详情页面TabDetailPager布局

--- app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
@@ -1,13 +1,18 @@
 package com.atguigu.myapplication.detailpager;
 
 import android.content.Context;
-import android.graphics.Color;
-import android.view.Gravity;
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import com.atguigu.myapplication.R;
 import com.atguigu.myapplication.base.MenuDetailBasePager;
 import com.atguigu.myapplication.domain.NewsCenterBean;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 /**
  * 作者：杨光福 on 2017/6/5 09:24
@@ -18,7 +23,15 @@ import com.atguigu.myapplication.domain.NewsCenterBean;
 
 public class TabDetailPager extends MenuDetailBasePager {
     private final NewsCenterBean.DataBean.ChildrenBean childrenBean;
-    private TextView textView;
+    @InjectView(R.id.viewpager)
+    ViewPager viewpager;
+    @InjectView(R.id.tv_title)
+    TextView tvTitle;
+    @InjectView(R.id.ll_point_group)
+    LinearLayout llPointGroup;
+    @InjectView(R.id.lv)
+    ListView lv;
+
     public TabDetailPager(Context context, NewsCenterBean.DataBean.ChildrenBean childrenBean) {
         super(context);
         this.childrenBean = childrenBean;
@@ -26,17 +39,16 @@ public class TabDetailPager extends MenuDetailBasePager {
 
     @Override
     public View initView() {
+        View view = View.inflate(context, R.layout.pager_tab_detail, null);
+        ButterKnife.inject(this,view);
         //创建子类的视图
-        textView = new TextView(context);
-        textView.setGravity(Gravity.CENTER);
-        textView.setTextColor(Color.RED);
-        return textView;
+        return view;
     }
 
     @Override
     public void initData() {
         super.initData();
         //设置数据
-        textView.setText(childrenBean.getTitle());
+        tvTitle.setText(childrenBean.getTitle());
     }
 }
