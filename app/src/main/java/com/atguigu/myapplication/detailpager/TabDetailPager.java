409b8f4c709fc17ff595dc4f23f4f0eb0272e219
yangguangfu520 <ygf@atguigu.com>
2017-06-06 09:21:34 +0800

    添加声音

--- app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
+++ app/src/main/java/com/atguigu/myapplication/detailpager/TabDetailPager.java
@@ -25,6 +25,7 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.google.gson.Gson;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.handmark.pulltorefresh.library.PullToRefreshListView;
+import com.handmark.pulltorefresh.library.extras.SoundPullEventListener;
 import com.zhy.http.okhttp.OkHttpUtils;
 import com.zhy.http.okhttp.callback.StringCallback;
 
@@ -81,6 +82,14 @@ public class TabDetailPager extends MenuDetailBasePager {
 
         //得到ListView
         lv = pull_refresh_list.getRefreshableView();
+        /**
+         * Add Sound Event Listener
+         */
+        SoundPullEventListener<ListView> soundListener = new SoundPullEventListener<ListView>(context);
+        soundListener.addSoundEvent(PullToRefreshBase.State.PULL_TO_REFRESH, R.raw.pull_event);
+        soundListener.addSoundEvent(PullToRefreshBase.State.RESET, R.raw.reset_sound);
+        soundListener.addSoundEvent(PullToRefreshBase.State.REFRESHING, R.raw.refreshing_sound);
+        pull_refresh_list.setOnPullEventListener(soundListener);
 
         //顶部的视图
         View viewTopNews = View.inflate(context, R.layout.tab_detail_topnews, null);
