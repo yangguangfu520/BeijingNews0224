7a24064dfd47bf23e33f6c4384a2a4f2b7357883
yangguangfu520 <ygf@atguigu.com>
2017-06-06 10:42:42 +0800

    使用WebView加载网页

--- app/src/main/java/com/atguigu/myapplication/activity/NewsDetailActivity.java
+++ app/src/main/java/com/atguigu/myapplication/activity/NewsDetailActivity.java
@@ -1,9 +1,12 @@
 package com.atguigu.myapplication.activity;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -31,14 +34,41 @@ public class NewsDetailActivity extends AppCompatActivity {
     WebView webview;
     @InjectView(R.id.progressbar)
     ProgressBar progressbar;
+    private Uri url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_detail);
         ButterKnife.inject(this);
-
         setView();
+        //网页地址
+        url = getIntent().getData();
+
+        WebSettings settings = webview.getSettings();
+        //设置相关配置
+        //设置支持javaScript
+        settings.setJavaScriptEnabled(true);
+        //设置双击页面变大变小
+        settings.setUseWideViewPort(true);
+
+        //添加变大变小按钮
+        settings.setBuiltInZoomControls(true);
+
+        //设置加载网页完成的监听
+        webview.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                progressbar.setVisibility(View.GONE);
+            }
+        });
+
+        //加载网页地址
+        webview.loadUrl(url.toString());
+//        webview.loadUrl("http://126.com/");
+
+
     }
 
     private void setView() {
